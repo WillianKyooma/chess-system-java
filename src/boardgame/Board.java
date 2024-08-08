0190ad00cccfb65c3f482070ab33e5f36d9e6a22
WillianKyooma <williancoden@gmail.com>
2024-08-08 18:35:38 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -54,6 +54,21 @@ private int rows;
 		piece.position = position;
 	} 
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		} 
+		if(piece(position)== null) {
+			return null;
+		} 
+		Piece aux = piece(position);
+		aux.position = null; 
+		pieces[position.getRow()][position.getColumn()] = null; 
+		return aux;
+	}
+	
+	
+	
 	private boolean positionExists(int row, int column) {
 	return	row >= 0 && row < rows && column >= 0 && column < columns;
 	}
@@ -67,5 +82,8 @@ private int rows;
 			throw new BoardException("Position not on the board");
 		}
 		return piece(position) != null;
-		}
+		} 
+	
+	
+	
 }
