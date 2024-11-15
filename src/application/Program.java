e029f27f88f4f5201cc3398e8ec08d98d53c621e
WillianKyooma <williancoden@gmail.com>
2024-11-14 22:07:28 -0300

    Printing possible moves

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,37 +12,35 @@ import chess.ChessPosition;
 
 public class Program {
  
-public static void main(String[]args) {
-	Scanner sc = new Scanner(System.in);
-	ChessMatch chessMatch = new ChessMatch(); 
-	
-	
-	
-	while(true) { 
-	try { 
-		UI.clearScreen();
-		UI.printBoard(chessMatch.getpieces()); 
-		System.out.println(); 
-		System.out.print("Source :"); 
-		ChessPosition source = UI.readChessPosition(sc); 
-	
-	System.out.println(); 
-	System.out.print("Target: ");
-	ChessPosition target = UI.readChessPosition(sc); 
-	
-	boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-	UI.clearScreen();
-	UI.printBoard(chessMatch.getpieces(),possibleMoves);
-	ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-	   }  
-	   catch(ChessException e) {
-		  System.out.print(e.getMessage()); 
-		  sc.nextLine();
-	   } 
-	 catch(InputMismatchException e) {
-		  System.out.print(e.getMessage()); 
-		  sc.nextLine();
-	   }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    ChessMatch chessMatch = new ChessMatch();
+
+	    while (true) {
+	        try {
+	            UI.clearScreen();
+	            UI.printBoard(chessMatch.getpieces());
+	            System.out.println();
+
+	            System.out.print("Source: ");
+	            ChessPosition source = UI.readChessPosition(sc);
+
+	            boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+	            UI.clearScreen();
+	            UI.printBoard(chessMatch.getpieces(), possibleMoves);
+	            System.out.println();
+
+	            System.out.print("Target: ");
+	            ChessPosition target = UI.readChessPosition(sc);
+
+	            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+	        } catch (ChessException e) {
+	            System.out.println(e.getMessage());
+	            sc.nextLine();
+	        } catch (InputMismatchException e) {
+	            System.out.println(e.getMessage());
+	            sc.nextLine();
+	        }
+	    }
 	}
-}
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -7,67 +7,61 @@ import chess.Color;
 
 public class Rook extends ChessPiece {
 
-	public Rook(Board board, Color color) {
-		super(board, color);
-		// TODO Auto-generated constructor stub
-	}
- 
-	
-	
-@Override 
-public String toString() {
-	return "R";
-}
+    public Rook(Board board, Color color) {
+        super(board, color);
+    }
 
- 
+    @Override
+    public String toString() {
+        return "R";
+    }
 
-@Override
-public boolean[][] possibleMoves() {
-	boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()]; 
-	
-	Position p = new Position(0,0);
-	
-	//ABOVE
-	p.setValues(position.getRow()-1, position.getColumn());
-	while(getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)){
-	  mat[p.getRow()][p.getColumn()]= true;	 
-	  p.setRow(p.getRow()-1);
-	}
-	
-	//BELOW
-		p.setValues(position.getRow()+1, position.getColumn());
-		while(getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)){
-		  mat[p.getRow()][p.getColumn()]= true;	 
-		  p.setRow(p.getRow()+1);
-		}
-	
-	
-	if(getBoard().positionExists(p) && isThereOponentPiece(p)) {
-		 mat[p.getRow()][p.getColumn()]= true;
-	} 
-	
-	//LEFT
-		p.setValues(position.getRow(), position.getColumn() - 1);
-		while(getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)){
-		  mat[p.getRow()][p.getColumn()]= true;	 
-		  p.setColumn(p.getColumn()-1);
-		}
-		
-		if(getBoard().positionExists(p) && isThereOponentPiece(p)) {
-			 mat[p.getRow()][p.getColumn()]= true;
-		} 
-		
-		//RIGHT
-				p.setValues(position.getRow(), position.getColumn() + 1);
-				while(getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)){
-				  mat[p.getRow()][p.getColumn()]= true;	 
-				  p.setColumn(p.getColumn() + 1);
-				}
-				
-				if(getBoard().positionExists(p) && isThereOponentPiece(p)) {
-					 mat[p.getRow()][p.getColumn()]= true;
-				}
-	
-	return mat;
-}
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // Acima
+        p.setValues(position.getRow() - 1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Abaixo
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Esquerda
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while (getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        // Direita
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while (getBoard().positionExists(p) && !getBoard().ThereIsAPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if (getBoard().positionExists(p) && isThereOponentPiece(p)) {
+            mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        return mat;
+    }
 }
